ac3a43e9f22c9f1755986d6a00e6c836464a7de7
johne-lima <johne.keyvid0@gmail.com>
2023-07-11 16:02:34 -0300

    começando o js

--- src/js/index.js
+++ src/js/index.js
@@ -22,12 +22,18 @@ const personagens = document.querySelectorAll('.personagem');
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
 
-        personagem.classList.add('selecionado');
-
-        const personagemSelecionado = document.querySelector('selecionado');
+        const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado');
 
-        const personagemGrande = document.querySelector('personagem-grande')
+        personagem.classList.add('selecionado');
 
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
     })
 })
